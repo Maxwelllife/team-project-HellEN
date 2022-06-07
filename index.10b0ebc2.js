c32beb5690ec174b52a23bbaddd4ae3490ae5f31
Maxwelllife <37122707+Maxwelllife@users.noreply.github.com>
2022-06-07 15:49:04 +0000

    Deploying to gh-pages from @ Maxwelllife/team-project-HellEN@b6d803923f6559b7ea54a4d852e6df063cd703f3 🚀

---
+++ index.10b0ebc2.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),Menu:document.querySelector("[data-menu]")};function n(){document.body.classList.toggle("menu-open"),e.Menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.10b0ebc2.js.map
--- index.1d7877e5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.1d7877e5.js.map
