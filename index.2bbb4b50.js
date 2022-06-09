1bcdfabe81f8a39321a7aafd6c0633b9b283f6bc
Maxwelllife <37122707+Maxwelllife@users.noreply.github.com>
2022-06-09 05:10:17 +0000

    Deploying to gh-pages from @ Maxwelllife/team-project-HellEN@55d52bc9443639fa3b13383a6d53d9c6413ab19a 🚀

---
+++ index.2bbb4b50.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:30,loop:!0,pagination:{el:".swiper-pagination",dynamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.2bbb4b50.js.map
--- index.f0f50adc.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:30,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.f0f50adc.js.map
