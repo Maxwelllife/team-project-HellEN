ee45442002f04f6ca84a99ef1d3bf9305176c421
Vlad Shchelkunov <specter.ode@gmail.com>
2022-06-09 14:23:29 +0300

    rev

--- src/js/more.js
+++ src/js/more.js
@@ -3,13 +3,13 @@ function readMore() {
     const more = document.getElementById("more");
     const btnMore = document.getElementById("btnMore");
 if(dots.style.display === "none") {
-    dots.style.display="inline";
-    btnMore.innerHTML="Hide";
+    dots.style.display="block";
+    btnMore.innerHTML="Read more";
     more.style.display="none";
 }
 else {
     dots.style.display="none";
     btnMore.innerHTML="Hide";
-    more.style.display="inline";
+    more.style.display="block";
 }
 }
