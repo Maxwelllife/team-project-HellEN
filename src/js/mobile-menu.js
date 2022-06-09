244485c9755b25d48fe80f95069ce97ae5ca9d28
Maksym Pokulytyi <mlightpro@gmail.com>
2022-06-09 10:03:43 +0300

    update mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,13 +3,18 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     Menu: document.querySelector('[data-menu]'),
+    menuList: document.querySelector('.menu__list'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
+  refs.menuList.addEventListener('click', removeMenu);
   function toggleMenu() {
     document.body.classList.toggle('menu-open');
     refs.Menu.classList.toggle('is-open');
+  }
+  function removeMenu() {
+    document.body.classList.remove('menu-open');
+    refs.Menu.classList.add('is-open');
   }
 })();
