b98169ad1c3488069de944af2bf547d0bb1e6ad7
Maxwelllife <37122707+Maxwelllife@users.noreply.github.com>
2022-06-09 13:32:20 +0000

    Deploying to gh-pages from @ Maxwelllife/team-project-HellEN@8539e1adf277696d9742b967b205c0899602258d 🚀

--- index.5c73663d.js
+++
@@ -1,2 +0,0 @@
-function readMore(){var e=document.getElementById("dots"),n=document.getElementById("more"),t=document.getElementById("btnMore");"none"===e.style.display?(e.style.display="block",t.innerHTML="Read more",n.style.display="none"):(e.style.display="none",t.innerHTML="Hide",n.style.display="block")}
-//# sourceMappingURL=index.5c73663d.js.map
