87e3da3178717b215e19dc073e0683d831bd666a
Maksym Pokulytyi <mlightpro@gmail.com>
2022-06-11 16:55:18 +0300

    add arrow to up

---
+++ src/js/scrollUp.js
@@ -0,0 +1,16 @@
+type =
+  'text/javascript' >
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+      } else {
+        $('.scrollup').fadeOut();
+      }
+    });
+
+    $('.scrollup').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 600);
+      return false;
+    });
+  });
