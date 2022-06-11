8b7c0c99f2061a335487fb44c1be3508d06eee3d
Maxwelllife <37122707+Maxwelllife@users.noreply.github.com>
2022-06-11 13:56:26 +0000

    Deploying to gh-pages from @ Maxwelllife/team-project-HellEN@63cce7d3118eaae3645f07f4c086081dc453d706 🚀

---
+++ index.373f0501.js
@@ -0,0 +1,2 @@
+type="text/javascript">$(document).ready((function(){$(window).scroll((function(){$(this).scrollTop()>100?$(".scrollup").fadeIn():$(".scrollup").fadeOut()})),$(".scrollup").click((function(){return $("html, body").animate({scrollTop:0},600),!1}))}));
+//# sourceMappingURL=index.373f0501.js.map
